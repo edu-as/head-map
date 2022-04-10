6b083f75f7e72e8f829afd16a85d2fddacab5044
edu-as <edu-as@hotmail.com>
2022-04-10 02:00:51 -0300

    make component app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <h1>High</h1>
+      <Title text="Entregue em qualquer lugar" />
+      <Title text="Cadastre-se e começe a usar" />
+      <Title text="Um market place com Avaliação de seus usuarios" />
     </div>
   );
 }
