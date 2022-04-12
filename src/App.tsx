cdb3507f43341a8d8e3e1d05cdb6f72ecae99918
edu-as <edu-as@hotmail.com>
2022-04-12 02:22:18 -0300

    adding image files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
+import './styles/global.css';
 interface TitleProps {
   text: string;
 }
 
 function Title(props: TitleProps) {
-  return (
-    <h1>{props.text}</h1>
-  )
+  return <h1>{props.text}</h1>;
 }
 
 function App() {
